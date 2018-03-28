3622a8da4abade40ebac67f6da7feb423ff26e20
Mark J Chen <mjchen.gene@gmail.com>
2018-03-28 10:00:58 -0500

    fix: generate_internal_data() function

--- R/data.R
+++ R/data.R
@@ -17,14 +17,7 @@ generate_internal_data <- function() {
       xreactive_probes_epic_pidsley,
       xreactive_probes_epic_mccartney
     ))
-  # Probes overlapping with genetic variants EPIC/850K
-  snp_probes_epic_pidsley <- loci_with_snps_epic_pidsley
-  snp_probes_epic <- unique(c(snp_probes_epic_pidsley))
-  # Probes overlapping with genetic variants 450K
-  snp_probes_450k <- character() # placeholder
   devtools::use_data(xreactive_probes_450k,
                      xreactive_probes_epic,
-                     snp_probes_epic,
-                     snp_probes_450k,
                      internal = TRUE)
 }
